c777ef8a477c46df52618567c8f6ec7d730b6d2f
Nolv <nolvenon@gmail.com>
2020-03-30 15:42:45 +0300

    Main activity; fragments refactor; trash removed

---
+++ app/src/main/java/com/example/halp/ReservedFragment.kt
@@ -0,0 +1,20 @@
+package com.example.halp
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class ReservedFragment : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_reserved_list, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
