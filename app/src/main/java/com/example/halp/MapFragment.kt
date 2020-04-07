ce7e3d4b0b6afa677e25319fe7c186249028800d
ierikhon <kinglordlear@gmail.com>
2020-04-07 15:46:25 +0300

    Errors fixed for now

--- app/src/main/java/com/example/halp/MapFragment.kt
+++ app/src/main/java/com/example/halp/MapFragment.kt
@@ -1,44 +1,16 @@
 package com.example.halp
 
-import android.os.Bundle;
+import android.os.Bundle
+import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.fragment.app.Fragment
-
-import com.yandex.mapkit.Animation;
-import com.yandex.mapkit.MapKitFactory;
-import com.yandex.mapkit.geometry.Point;
-import com.yandex.mapkit.map.CameraPosition;
-
-import com.yandex.mapkit.mapview.MapView;
-
 
 class MapFragment : Fragment() {
 
-    private val MAPKIT_API_KEY = "161a85a8-6f5e-475b-9b54-7e43cb5cde73"
-    private val TARGET_LOCATION: Point = Point(59.945933, 30.320045)
-
-    private var mapView: MapView? = null
-
     override fun onCreate(savedInstanceState: Bundle?) {
-
-
-        MapKitFactory.setApiKey(MAPKIT_API_KEY)
-        MapKitFactory.initialize(this.context)
-        setContentView(R.layout.fragment_map)
         super.onCreate(savedInstanceState)
         setHasOptionsMenu(false);
-        mapView = findViewById(R.id.mapview) as MapView
-
-        mapView!!.map.move(
-            CameraPosition(TARGET_LOCATION, 14.0f, 0.0f, 0.0f),
-            Animation(Animation.Type.SMOOTH, 5F),
-            null
-        )
-
-
-
     }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
@@ -50,17 +22,5 @@ class MapFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-    }
-
-    override fun onStop() {
-        mapView!!.onStop()
-        MapKitFactory.getInstance().onStop()
-        super.onStop()
-    }
-
-    override fun onStart() {
-        super.onStart()
-        MapKitFactory.getInstance().onStart()
-        mapView!!.onStart()
     }
 }
