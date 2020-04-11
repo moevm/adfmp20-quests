6e22a9e4a0a0594a5e0221be843635eae85499fd
ierikhon <kinglordlear@gmail.com>
2020-04-11 13:17:58 +0300

    KMongo Dependiencies and blank Klassfile

---
+++ app/src/main/java/com/example/halp/Database.kt
@@ -0,0 +1,5 @@
+package com.example.halp
+
+import org.litote.kmongo.reactivestreams.*  //NEEDED! import KMongo reactivestreams extensions
+import org.litote.kmongo.coroutine.* //NEEDED! import KMongo coroutine extensions
+
