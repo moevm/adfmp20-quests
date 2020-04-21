2bcda9ec8a1aa2ba8aaa4ef609f3025346a68cc2
Nolv <nolvenon@gmail.com>
2020-04-21 05:43:57 +0300

    Where am i

--- app/src/main/java/com/example/halp/BookingFragment.kt
+++ app/src/main/java/com/example/halp/BookingFragment.kt
@@ -1,5 +1,6 @@
 package com.example.halp
 
+import android.app.Activity
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -7,17 +8,25 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.DatePicker
+import android.widget.Spinner
+import android.widget.TextView
 import androidx.navigation.findNavController
+import com.google.android.material.textfield.TextInputEditText
+import java.util.*
 
 class BookingFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         val view = inflater.inflate(R.layout.fragment_booking, container, false)
+        val act = activity as MainActivity
 
         view.findViewById<DatePicker>(R.id.booking_date_picker).minDate = System.currentTimeMillis() - 1000
 
         view.findViewById<Button>(R.id.booking_reserve_button).setOnClickListener { v ->
+
+            harverst(act);
+
             v.findNavController().navigate(R.id.action_bookingFragment_to_successFragment)
         }
 
@@ -26,5 +35,23 @@ class BookingFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        val act = activity as MainActivity
+
+        fillFields(act);
+    }
+
+    fun fillFields(act: MainActivity)
+    {
+        view?.findViewById<TextView>(R.id.booking_price)?.text = act.quest?.cost.toString()
+    }
+
+    fun harverst(act: MainActivity) {
+        val o: Order = Order()
+        o.comment = view?.findViewById<TextInputEditText>(R.id.booking_comment)?.text.toString()
+        o.people = view?.findViewById<TextInputEditText>(R.id.booking_people)?.text.toString().toInt()
+        o.quest_id = act.quest?.id
+        o.order_date = Date()
+        o.quest_date = view?.findViewById<DatePicker>(R.id.booking_date_picker)
+
     }
 }
---
+++ app/src/main/java/com/example/halp/Order.kt
@@ -0,0 +1,14 @@
+package com.example.halp
+
+import java.text.SimpleDateFormat
+import java.util.*
+
+class Order {
+    var quest_id: String? = null
+    var quest_date: Date? = null
+    var order_date: Date? = null;
+    var people = 0
+    var cost = 0f
+    var status: String? = null
+    var comment: String? = null
+}
