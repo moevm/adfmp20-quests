72096fbd7714791ad194570215dc49c28bf68f83
Piskunov Yaroslav <kinglordlear@gmail.com>
2020-04-22 19:52:39 +0300

    Unit Test 2 of 3

--- app/src/test/java/com/example/halp/ExampleUnitTest.kt
+++ app/src/test/java/com/example/halp/ExampleUnitTest.kt
@@ -1,8 +1,11 @@
 package com.example.halp
 
+import com.google.firebase.Timestamp
 import org.junit.Test
 
 import org.junit.Assert.*
+import java.util.*
+
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -13,5 +16,57 @@ class ExampleUnitTest {
     @Test
     fun addition_isCorrect() {
         assertEquals(4, 2 + 2)
+    }
+
+    @Test
+    fun ordersAdapter_correct_getItemCount(){
+        val sampleOrder = Order()
+        val activity = MainActivity()
+        val adapter = OrdersAdapter(act = activity, orders = arrayListOf())
+
+        assertEquals(adapter.getItemCount(), 0)
+
+        adapter.orders?.plusAssign(sampleOrder)
+
+        assertEquals(adapter.getItemCount(), 1)
+
+        for (i in 1..35)
+            adapter.orders?.plusAssign(sampleOrder)
+
+        assertEquals(adapter.getItemCount(), 36)
+
+        for (i in 1..33)
+            adapter.orders?.plusAssign(sampleOrder)
+
+        assertEquals(adapter.getItemCount(), 69)
+    }
+
+    @Test
+    fun order_correct_set(){
+        val order = Order()
+        val quest_id = "12345"
+        val quest_date = Timestamp(Date())
+        val order_date = Timestamp(Date())
+        val people = 5
+        val cost = 1325555
+        val status = "CoolStatus18"
+        val comment = ""
+
+        order.set("quest_id", quest_id)
+        order.set("quest_date", quest_date)
+        order.set("order_date", order_date)
+        order.set("people", people)
+        order.set("cost", cost)
+        order.set("status", status)
+        order.set("comment", comment)
+
+        assertEquals(order.quest_id, quest_id)
+        assertEquals(order.quest_date, quest_date.toDate())
+        assertEquals(order.order_date, order_date.toDate())
+        assertEquals(order.people, people)
+        assertEquals(order.cost, cost)
+        assertEquals(order.status, status)
+        assertEquals(order.comment, comment)
+
     }
 }
